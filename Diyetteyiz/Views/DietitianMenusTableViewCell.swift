da1af06cfae2701358ce9217750652530977dac0
Nizamet Özkan <nizamet005@gmail.com>
2021-03-10 14:49:19 +0300

    Bum

---
+++ Diyetteyiz/Views/DietitianMenusTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  DietitianMenusTableViewCell.swift
+//  Diyetteyiz
+//
+//  Created by Nizamet Özkan on 10.03.2021.
+//
+
+import UIKit
+
+class DietitianMenusTableViewCell: UITableViewCell {
+
+    public static let identifier = "DietitianMenusTableViewCell"
+
+}
